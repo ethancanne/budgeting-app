dd6ef67dc6e7ea0d95558791219f495996bfdf20
ZanyHunter <zanyhunter1@gmail.com>
2022-02-03 14:03:31 -0500

    Begin building Transaction.js and User.js Very much a work in progress.

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,13 +1,31 @@
 Transaction;
-date;
-amount;
-name;
-desc;
-category;
-isExpense;
+const date;
+const amount;
+const name;
+const desc;
+const category;
+const isExpense;
+
+const categoriesEnum = Object.freeze({
+  paycheck: 'Paycheck',
+  interest: 'Interest',
+  bill: 'Bills & Utilities',
+  food: 'Food',
+  rent: 'Rent / Mortgage',
+  auto: 'Auto & Transport',
+  shopping: 'Shopping',
+  entertainment: 'Entertainment',
+  gift: 'Gifts & Donations',
+  travel: 'Travel',
+  misc: 'Other'
+})
 
 class Transaction {
-  constructor(date) {
+  constructor(name, date, amount, category, isExpense) {
+    this.name = name;
     this.date = date;
+    this.amount = amount;
+    this.category = category;
+    this.isExpense = isExpense;
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,40 @@
 User;
-Transactions;
+transactions;
 totalIncome;
 totalExpense;
+const username;
 
 generateReport;
+
+class User {
+    constructor(username) {
+        this.username = username;
+    }
+
+    addTransaction(transaction) {
+        // Add transaction to total amount of money user has
+        if(transaction.isExpense) totalExpense += transaction.amount;
+        else totalIncome += transaction.amount;
+        transactions.push(transaction);
+    }
+
+    removeTransaction(transactionName) {
+        for(let i = transactions.length; i >=0; --i) {
+            if(transactions[i].name == transactionName) {
+                transactions.splice(i, 1);
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    recalculateTotals() {
+        totalIncome = totalExpense = 0;
+        for(let i = 0; i < transactions.length; i++) {
+            if(transactions[i].isExpense) totalExpense += transactions[i].amount;
+            totalIncome += transactions[i].amount;
+        }
+    }
+
+    generateReport(dateStart, dateEnd) {}
+}

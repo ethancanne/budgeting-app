71278cc8f2f89b222752524e29b0e153ce0368e1
Hunter Pugh <34586837+ZanyHunter@users.noreply.github.com>
2022-02-04 09:07:11 -0500

    Create Transaction form structure

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -6,20 +6,6 @@ const desc;
 const category;
 const isExpense;
 
-const categoriesEnum = Object.freeze({
-  paycheck: 'Paycheck',
-  interest: 'Interest',
-  bill: 'Bills & Utilities',
-  food: 'Food',
-  rent: 'Rent / Mortgage',
-  auto: 'Auto & Transport',
-  shopping: 'Shopping',
-  entertainment: 'Entertainment',
-  gift: 'Gifts & Donations',
-  travel: 'Travel',
-  misc: 'Other'
-})
-
 class Transaction {
   constructor(name, date, amount, category, isExpense) {
     this.name = name;
--- src/views/createTransactionView/CreateTransactionView.js
+++ src/views/createTransactionView/CreateTransactionView.js
@@ -3,10 +3,30 @@ import React, { useState } from "react";
 import Button from "../../core/button/Button";
 
 const CreateTransactionView = props => {
+  const [name, setName] = useState('Name');
+  const [date, setDate] = useState(0);
   const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState('Other');
+  const [isExpense, setIsExpense] = useState(0);
+
+  const updateName = e => {
+    setName(e.target.value);
+  }
+
+  const updateDate = e => {
+    setDate(e.target.value);
+  };
 
   const updateAmount = e => {
     setAmount(e.target.value);
+  };
+
+  const updateCategory = e => {
+    setCategory(e.target.value);
+  };
+
+  const updateIsExpense = e => {
+    setIsExpense(e.target.value);
   };
 
   //Save the transaction to local storage on form submit
@@ -15,15 +35,46 @@ const CreateTransactionView = props => {
     e.preventDefault();
     e.stopPropagation();
     //Create transaction object with state properties
+    const newTransaction = new Transaction(name, date, amount, category, isExpense);
     //Store the transaction object in local storage
+    App.user.transactions.push(newTransaction);
     //Close the popup
     props.setPopup(false);
   };
   return (
     <div className='create-transaction-view'>
       <form onSubmit={handleCreateTransaction}>
-        <input type='text' value={amount} onChange={updateAmount} />
-        <input type='text' value={amount} onChange={updateAmount} />
+        <label>Name
+          <input type='text' value={name} onChange={updateName} />
+        </label>
+        <label>Date
+          <input type='date' value={date} onChange={updateDate} />
+        </label>
+        <label>Amount
+          <input type='text' value={amount} onChange={updateAmount}
+          pattern="(0\.((0[1-9]{1})|([1-9]{1}([0-9]{1})?)))|(([1-9]+[0-9]*)(\.([0-9]{1,2}))?)"/>
+        </label>
+        <label>Category
+          <select value={isExpense} onChange={updateCategory}>
+            <option value='Paycheck'>Paycheck</option>
+            <option value='Interest'>Interest</option>
+            <option value='Bills & Utilities'>Bills & Utilities</option>
+            <option value='Food'>Food</option>
+            <option value='Rent & Mortgage'>Rent & Mortgage</option>
+            <option value='Auto & Transport'>Auto & Transport</option>
+            <option value='Shopping'>Shopping</option>
+            <option value='Entertainment'>Entertainment</option>
+            <option value='Gifts & Donations'>Gifts & Donations</option>
+            <option value='Travel'>Travel</option>
+            <option value='Other'>Other</option>
+          </select>
+        </label>
+        <label>Income or Expense
+          <select onChange={updateIsExpense}>
+            <option value='false'>Income</option>
+            <option value='true'>Expense</option>
+          </select>
+        </label>
 
         <Button>Click</Button>
       </form>

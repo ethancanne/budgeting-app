3ba1090a5bfa08c59e74aa84caca61581228f332
Ethan Cannelongo <ethancannelongo@Ethans-MacBook-Pro.local>
2022-02-07 14:23:41 -0500

    Finished graph
    Graph is now working, laying out all the transactions

--- src/views/reportView/ReportView.js
+++ src/views/reportView/ReportView.js
@@ -1,6 +1,16 @@
 import "./ReportView.scss";
 import User from "../../models/User";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 const ReportView = () => {
   const transactions = User.createFromObject(
@@ -8,103 +18,55 @@ const ReportView = () => {
   ).transactions;
 
   var maxValue = 0;
-  var categoryValues = [];
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log(transactions);
+    var theData = [];
+
     transactions.forEach(transaction => {
       if (transaction.amount > maxValue) {
         maxValue = transaction.amount;
       }
-      var categoryIndex = categoryValues.findIndex(
+
+      var categoryIndex = theData.findIndex(
         category => category.category === transaction.category
       );
-      // console.log(categoryValues);
-      if (!categoryIndex !== -1) {
-        categoryValues.push({
+
+      if (categoryIndex === -1) {
+        theData.push({
           category: transaction.category,
           income: 0,
           expense: 0,
         });
-        categoryIndex = categoryValues.length - 1;
+        categoryIndex = theData.length - 1;
       }
-      categoryValues[categoryIndex].income += transaction.isExpense
+
+      theData[categoryIndex].income += transaction.isExpense
         ? 0
         : transaction.amount;
-      categoryValues[categoryIndex].expense += transaction.isExpense
+      theData[categoryIndex].expense += transaction.isExpense
         ? transaction.amount
         : 0;
+
+      console.log(theData);
+
+      setData(theData);
     });
-    console.log(categoryValues, maxValue);
   }, []);
+
   return (
     <div className='report-view'>
-      <table id='moneygraph'>
-        <caption>Financial Report</caption>
-        <thead>
-          <tr>
-            <th></th>
-            <th className='income'>Income</th>
-            <th className='expense'>Expense</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className='qtr' id='q1'>
-            <th scope='row'>Q1</th>
-            <td className='income bar' style={{ height: "111px" }}>
-              <p>$18,450.00</p>
-            </td>
-            <td className='expense bar' style={{ height: "99px" }}>
-              <p>$16,500.00</p>
-            </td>
-          </tr>
-          <tr className='qtr' id='q2'>
-            <th scope='row'>Q2</th>
-            <td className='income bar' style={{ height: "206px" }}>
-              <p>$34,340.72</p>
-            </td>
-            <td className='expense bar' style={{ height: "194px" }}>
-              <p>$32,340.72</p>
-            </td>
-          </tr>
-          <tr className='qtr' id='q3'>
-            <th scope='row'>Q3</th>
-            <td className='income bar' style={{ height: "259px" }}>
-              <p>$43,145.52</p>
-            </td>
-            <td className='expense bar' style={{ height: "193px" }}>
-              <p>$32,225.52</p>
-            </td>
-          </tr>
-          <tr className='qtr' id='q4'>
-            <th scope='row'>Q4</th>
-            <td className='income bar' style={{ height: "110px" }}>
-              <p>$18,415.96</p>
-            </td>
-            <td className='expense bar' style={{ height: "195px" }}>
-              <p>$32,425.00</p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <div id='ticks'>
-        <div className='tick' style={{ height: "59px" }}>
-          <p>$50,000</p>
-        </div>
-        <div className='tick' style={{ height: "59px" }}>
-          <p>$40,000</p>
-        </div>
-        <div className='tick' style={{ height: "59px" }}>
-          <p>$30,000</p>
-        </div>
-        <div className='tick' style={{ height: "59px" }}>
-          <p>$20,000</p>
-        </div>
-        <div className='tick' style={{ height: "59px" }}>
-          <p>$10,000</p>
-        </div>
-      </div>
+      <ResponsiveContainer width={"100%"} height={"100%"}>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='category' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey='income' fill='#8884d8' />
+          <Bar dataKey='expense' fill='#82ca9d' />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/views/transactionHistoryView/TransactionHistoryView.js
+++ src/views/transactionHistoryView/TransactionHistoryView.js
@@ -3,33 +3,38 @@ import React, { useState } from "react";
 import User from "../../models/User";
 
 const TransactionHistoryView = props => {
-    const transactions = User.createFromObject(JSON.parse(localStorage.getItem("user"))).transactions;
+  var transactions = [];
+  if (localStorage.getItem("user")) {
+    transactions = User.createFromObject(
+      JSON.parse(localStorage.getItem("user"))
+    ).transactions;
+  }
 
-    return (
-        <div className='transaction-history-view'>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Date</th>
-                    <th>Amount</th>
-                    <th>Category</th>
-                    <th>Income or Expense</th>
-                    <th>Delete</th>
-                </tr>
-                
-                {transactions.map(transaction => (
-                    <tr>
-                        <td>{transaction.name}</td>
-                        <td>{transaction.date}</td>
-                        <td>${transaction.amount}</td>
-                        <td>{transaction.category}</td>
-                        <td>{transaction.isExpense ? 'Expense' : 'Income'}</td>
-                        <td>Delete</td>                      
-                    </tr>
-                ))}
-            </table>
-        </div>
-    )
-}
+  return (
+    <div className='transaction-history-view'>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th>Income or Expense</th>
+          <th>Delete</th>
+        </tr>
+
+        {transactions.map(transaction => (
+          <tr>
+            <td>{transaction.name}</td>
+            <td>{transaction.date}</td>
+            <td>${transaction.amount}</td>
+            <td>{transaction.category}</td>
+            <td>{transaction.isExpense ? "Expense" : "Income"}</td>
+            <td>Delete</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
 
 export default TransactionHistoryView;

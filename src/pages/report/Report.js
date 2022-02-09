3ffd0af60a356c461492d76f8ad75796dbdc12b3
Conor Helmick <chelmick1@liberty.edu>
2022-02-09 09:17:48 -0500

    adding report range functionality

--- src/pages/report/Report.js
+++ src/pages/report/Report.js
@@ -1,32 +1,37 @@
 import "./Report.scss";
-import React from "react";
+import React, { useState } from "react";
 import Page from "../Page";
 import RangeView from "../../views/RangeView/RangeView";
 import ReportView from "../../views/reportView/ReportView";
 
 const Report = () => {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const updateStartDate = (e)=>{
+    setStartDate(e.target.value)
+  };
+
+  const updateEndDate = (e)=>{
+    setEndDate(e.target.value)
+  };
+
   return (
     <Page>
       <div className='report-container'>
-<<<<<<< HEAD
-        <ReportView monthRange={[0, 4]} year={2022} />
-        <div className='range-container'>
-          <RangeView />
-        </div>
-=======
         <ReportView
           dateRange={[
             new Date(
-              "Wed Feb 07 2022 08:34:43 GMT-0500 (Eastern Standard Time)"
+              startDate
             ),
             new Date(
-              "Wed Feb 20 2022 08:34:43 GMT-0500 (Eastern Standard Time)"
+              endDate
             ),
           ]}
           year={2022}
         />
->>>>>>> fcc02e454b80af1275a6479a332ff0d02c790afc
       </div>
+      <div><RangeView startDate={startDate} updateStartDate={updateStartDate} endDate={endDate} updateEndDate={updateEndDate}/></div>
       <div className='button-container'></div>
     </Page>
   );
--- src/views/RangeView/RangeView.js
+++ src/views/RangeView/RangeView.js
@@ -1,17 +1,17 @@
 import "./RangeView.scss";
 import React from "react";
 
-const RangeView = () => {
+const RangeView = props => {
     return (
         <div>
             <form>
                 <label>
                     <p>Start Date</p>
-                    <input type='date' value={date}></input>
+                    <input type='date' value={props.startDate} onChange={props.updateStartDate}></input>
                 </label>
                 <label>
                     <p>End Date</p>
-                    <input type='date' value={date}></input>
+                    <input type='date' value={props.endDate} onChange={props.updateEndDate}></input>
                 </label>
             </form>
         </div>
